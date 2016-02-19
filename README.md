0f60a592cfbf457a878f67a45e7948f6a991bf76
Stephen Salzer <stephen.salzer@gmail.com>
2016-02-18 18:59:06 -0800

    commit of tcp server

---
+++ README.md
@@ -0,0 +1,1 @@
+# 401_tcp_server
---
+++ server.js
@@ -0,0 +1,17 @@
+var net = require('net');
+var fs = require('fs');
+var fileCount = 0;
+
+var server = module.exports = exports = net.createServer((socket) => {
+  var requestTime = new Date();
+  var filename = fileCount++ + '.log';
+  var file= fs.createWriteStream(__dirname + '/log/' + filename);
+  socket.pipe(file);
+  socket.write(filename);
+  socket.end();
+  socket.on('close', () => {
+    file.close();
+  });
+});
+
+server.listen(3000, () => console.log('server up'));
